d7e62f1e6272d59222f1678faaed34aeae808506
Darren Cook <thelurkingdev@gmail.com>
2018-12-17 23:05:13 -0500

    Some text styling.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi in voluptas consequuntur. Modi adipisci ipsum culpa officiis assumenda provident, nobis quidem ab facere blanditiis delectus et quae odit aliquid?</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quibusdam consequuntur cumque beatae architecto nam porro placeat itaque eum nisi dolor provident, iusto voluptate, nobis nemo? Earum laudantium officia facilis.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline.</p>
+    <p class="subheading">This is a subheading.</p>
+    <p class="caption">This is a caption.</p>
   </div>
 </template>
 
