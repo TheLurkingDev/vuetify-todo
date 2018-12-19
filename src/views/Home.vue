ae40abf48a3adf1d46f8233f775ab3760d7c3f77
Darren Cook <thelurkingdev@gmail.com>
2018-12-18 20:43:44 -0500

    Buttons and icons.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi in voluptas consequuntur. Modi adipisci ipsum culpa officiis assumenda provident, nobis quidem ab facere blanditiis delectus et quae odit aliquid?</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quibusdam consequuntur cumque beatae architecto nam porro placeat itaque eum nisi dolor provident, iusto voluptate, nobis nemo? Earum laudantium officia facilis.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller Display</h4>
-    <p class="headline">This is a headline.</p>
-    <p class="subheading">This is a subheading.</p>
-    <p class="caption">This is a caption.</p>
+   <v-btn class="pink white--text">Click Me</v-btn>
+   <v-btn depressed color="pink">Click Me</v-btn>
+   <v-btn flat color="pink">Click Me</v-btn>
+   <v-btn depressed color="pink white--text">
+     <v-icon left>email</v-icon>
+     <span>email me</span>
+   </v-btn>
+   <v-btn depressed small color="pink white--text">
+     <v-icon left small>email</v-icon>
+     <span>email me</span>
+   </v-btn>
+   <v-btn depressed large color="pink white--text">     
+     <span>email me</span>
+     <v-icon right large>email</v-icon>
+   </v-btn>
+   <v-btn fab depressed small dark color="purple white--text">          
+     <v-icon>favorite</v-icon>
+   </v-btn>
   </div>
 </template>
 
