8e4bf84fb0e05498508fa819d0acf91353af1f6a
Darren Cook <thelurkingdev@gmail.com>
2018-12-18 21:09:45 -0500

    Breakpoints and visibility.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,9 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-   <v-btn class="pink white--text">Click Me</v-btn>
-   <v-btn depressed color="pink">Click Me</v-btn>
-   <v-btn flat color="pink">Click Me</v-btn>
-   <v-btn depressed color="pink white--text">
-     <v-icon left>email</v-icon>
-     <span>email me</span>
-   </v-btn>
-   <v-btn depressed small color="pink white--text">
-     <v-icon left small>email</v-icon>
-     <span>email me</span>
-   </v-btn>
-   <v-btn depressed large color="pink white--text">     
-     <span>email me</span>
-     <v-icon right large>email</v-icon>
-   </v-btn>
-   <v-btn fab depressed small dark color="purple white--text">          
-     <v-icon>favorite</v-icon>
-   </v-btn>
+    <v-btn class="hidden-sm-and-down" color="red">Hide On Small</v-btn>
+    <v-btn class="hidden-md-and-up" color="blue">Hide On Large</v-btn>
+    <v-btn class="hidden-sm-only" color="green">Hide On Small Only</v-btn>
   </div>
 </template>
 
