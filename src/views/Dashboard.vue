3fef1707c7a6827c032bf9f1b8c6b7fc072fbc35
Darren Cook <thelurkingdev@gmail.com>
2018-12-19 22:38:33 -0500

    Created grid layout for Dashboard.vue.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,57 +3,26 @@
     <h1 class="subheading grey--text">{{ name }}</h1>
 
     <v-container class="my-5">
-      <v-layout row wrap>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>          
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">2</v-btn>          
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">3</v-btn>          
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">4</v-btn>          
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-space-between>
-        <v-flex xs4 md3>
-          <v-btn outline block class="navdrawer">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="navdrawer">2</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-space-around>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">2</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-center>
-        <v-flex xs4 md3>
-          <v-btn outline block class="info">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="info">2</v-btn>
-        </v-flex>
-      </v-layout>
-      
-      <v-layout row wrap justify-end>
-        <v-flex xs4 md3>
-          <v-btn outline block class="error">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="error">2</v-btn>
-        </v-flex>
-      </v-layout>
-
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>Darren Cook</div>
+          </v-flex>        
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 2019</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>    
   </div>
 </template>
