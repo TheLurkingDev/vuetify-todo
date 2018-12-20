c7dd5ad77877a7a6f6fc1a1d509d57849650e119
Darren Cook <thelurkingdev@gmail.com>
2018-12-19 22:20:50 -0500

    Added grid system examples.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,10 +3,58 @@
     <h1 class="subheading grey--text">{{ name }}</h1>
 
     <v-container class="my-5">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora omnis facilis cumque vel obcaecati accusantium adipisci voluptate, sed iure eum assumenda, praesentium recusandae ipsa, a consequuntur saepe doloremque temporibus quidem!</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat quos delectus harum voluptates, ducimus blanditiis cum nisi numquam, quibusdam iusto recusandae ullam error, sunt nam enim. Recusandae beatae quibusdam soluta.</p>
-    </v-container>
-    
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">1</v-btn>          
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>          
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">3</v-btn>          
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">4</v-btn>          
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-space-between>
+        <v-flex xs4 md3>
+          <v-btn outline block class="navdrawer">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="navdrawer">2</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-space-around>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">2</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-center>
+        <v-flex xs4 md3>
+          <v-btn outline block class="info">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="info">2</v-btn>
+        </v-flex>
+      </v-layout>
+      
+      <v-layout row wrap justify-end>
+        <v-flex xs4 md3>
+          <v-btn outline block class="error">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="error">2</v-btn>
+        </v-flex>
+      </v-layout>
+
+    </v-container>    
   </div>
 </template>
 
