6cdb75c14b56c738a14aeb5f916eae6f0bdf3b33
Darren Cook <thelurkingdev@gmail.com>
2018-12-22 15:54:59 -0500

    Added a popup 'v-dialog' component.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,6 +31,9 @@
                     </v-avatar>
                     <p class="white--text subheading mt-1">The Net Ninja</p>
                 </v-flex>
+                <v-flex class="mt-4 mb-3">
+                    <popup />
+                </v-flex>
             </v-layout>
             <v-list>
                 <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
@@ -47,7 +50,12 @@
 </template>
 
 <script>
+import Popup from './Popup';
+
 export default {
+    components: {
+        Popup
+    },
     data() {
         return {
             drawer: false,
